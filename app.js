e3a34e24f99f29774ab0b21bb178f2896d28af36
leeannehammond <56901618+leeannehammond@users.noreply.github.com>
2020-01-15 12:34:04 -0500

    w05d1_homework

--- app.js
+++ app.js
@@ -1,124 +1,77 @@
-console.log('Landscaper Game app.js is attached to index.html');
-
-// INITIALIZE
-let tool;
-let money;
-let cost;
-let choice;
-
-let start = () => {
-  tool = 'your teeth';
-  money = 1;
-  askForAction();
-};
+// HELLO! I attempted to do a shift array for the battle and am unsure of where
+// exactly it should go or what other code to put before it!!!!
 
 
-// USER INFORMATION
-let showStatus = () => {
-  alert('You have $' + money + '.  Your tool is ' + tool + '.')
-};
 
-// USER ACTIONS AND INPUT
-let askForAction = () => {
-  showStatus();
-  promptAlerts();
-};
-
-let promptAlerts = () => {
-  choice = prompt('What do you want to do?', 'landscape/buy tools');
-    if (choice.toLowerCase() === 'landscape') {
-      landscape();
-    } else if (choice.toLowerCase() === 'buy tools') {
-      checkTool();
-    } else if (choice.toLowerCase() === 'exit') {
-      //exits game - option not shown to user
-    } else if (choice.toLowerCase() === 'restart') {
-      //resets game - option not shown to user
-      start();
+// Create the Player's ship object:
+let playerShip = {
+  hull: 20,
+  firepower: 5,
+  accuracy: 0.7,
+  // Add methods 
+  attack: enemyShip => {
+    console.log("Player attacking Alien ship");
+    // Check for hit  on playerShip accuracy:
+    if (Math.random() < playerShip.accuracy) {
+      console.log("Attack hit!");
+      enemyShip.hull -= playerShip.firepower;
     } else {
-      alert('Please try that action again!')
-      askForAction();
+      console.log("Attack missed!");
     }
-};
-
-//LANDSCAPE ACTIONS
-let landscape = () => {
-  if (tool === 'your teeth') {
-    alert('Using ' + tool + ' will earn you $1 each day!')
-    money += 1;
-    askForAction();
-  } else if (tool === 'a pair of rusty scissors') {
-    money += 5;
-    askForAction();
-  } else if (tool === 'an old-timey push lawnmower') {
-    money += 50;
-    askForAction();
-  } else if (tool === 'a fancy battery-powered lawnmower') {
-    money += 100;
-    askForAction();
-  } else if (tool === 'a team of starving students') {
-    money += 250;
-    if (money <1000) {
-      askForAction();
+    console.log(enemyShip);
+     //if (Math.random() < alien[0].accuracy) {
+     //console.log('You have been hit!');
+  }
+}
+//  AlienShip class
+class AlienShip {
+  constructor() {
+    // Make hull random between 3 and 6
+    this.hull = Math.floor(Math.random() * 4 + 3);
+    // Make random between 2 and 4
+    this.firepower = Math.floor(Math.random() * 3 + 2);
+    // TODO: Make random between .6 and .8
+    this.accuracy = Math.floor(Math.random() * 3 + 6) * 0.1;
+  }
+  attack() {
+    console.log("Alien Ship attacking");
+    // Check for hit or miss:
+    if (Math.random() < this.accuracy) {
+      console.log("Alien ship hit player!");
+      playerShip.hull -= this.firepower;
+      console.log(`Player Ship hull remaining: ${playerShip.hull}`);
     } else {
-    // Game Ends
-      alert('Congratulations!  You have made $' + money + ' with the help of your tools!  You have won the game!')
+      console.log("Alien ship missed");
     }
   }
-};
-
-
-// CHECK AND BUY TOOLS
-let checkTool = function () {
-  if (money < 5) {
-    alert('Sorry, you don\'t have enough money to buy a new tool yet.  Keep landscaping!');
-    askForAction();
-  } else {
-    buyTool();
+}
+// Start 
+console.log("Generating enemy ships");
+enemy = new AlienShip();
+console.log(enemy);
+// Create a game object - checkwin
+// Turn by turn logic
+// Player ship attacks:
+playerShip.attack(enemy);
+// Check if enemy ship is destroyed:
+if (enemy.hull <= 0) {
+  console.log("Enemy ship destroyed!");
+  let response = prompt("Ship Destroyed, attack or retreat?");
+  if (response === "retreat") {
+    alert("You retreated, game over");
+  } else if (response === "attack") {
+    // TODO: finish this
+    // do attack
+    console.log("Continuing gameplay");
   }
-};
+} else {
+  // Enemy ship attacks:
+  enemy.attack();
+  // shift array--???? 
+}
+if (fightOneAlien.hull <= 0) {
+  // If enemy ship is destroyed:
+  alienEnemies.shift(); 
 
-// functions
-let buyScissors = () => {
-  cost = 5;
-  money -= 5;
-  makeAmount = 5;
-  tool = 'a pair of rusty scissors';
-  alertText();
-};
-
-let buyPushMower= () => {
-  cost = 25;
-  money -= 25;
-  makeAmount = 50;
-  tool = 'an old-timey push lawnmower';
-  alertText();
-};
-
-let buyFancyLawnMower = () => {
-  cost = 250;
-  money -= 250;
-  makeAmount = 100;
-  tool = 'a fancy battery-powered lawnmower';
-  alertText();
-};
-
-let buyTeamOfStudents = () => {
-  cost = 500;
-  money -= 500;
-  makeAmount = 250;
-  tool = 'a team of starving students';
-  alertText();
-};
-
-let alertText = () => {
-  alert('You can buy ' + tool + ' for $' + cost + '.');
-  alert('You have purchased ' + tool + '!  Using this tool will earn you $' + makeAmount + ' each day!');
-  askForAction();
-};
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  start();
-});
+// Retreat option
+// End game logic
