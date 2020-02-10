e1754436ce1b947e40d84c33424dd629a2346aba
leeannehammond <56901618+leeannehammond@users.noreply.github.com>
2020-02-10 12:43:44 -0500

    tip calculator express app created

--- w09d2_homework/server.js
+++ w09d2_homework/server.js
@@ -9,9 +9,9 @@ const app = express();
 // listen for when someone goes to
 // localhost:2000/
 
+
 const express = require('express'); //from documentation: express is function
 const app = express();//app is an object
-
 app.get('/greeting', (req, res)=>{
   res.send('Hello Stranger!');
 });
@@ -23,4 +23,13 @@ app.get('/greeting/'), (req,res) => {
 
 app.listen(2000, () => {
     console.log("Wow! Hello there");
+});
+
+//////////////// Tip Calculator ////////////////
+
+// app route of '/tip' w/2 params of total and tipPercentage
+// hit route to display how much tip is w/total amt and tip amt
+app.get('/ti[\p/:total/:tipPercentage', (req, res) => {
+	console.log(req.params);
+	res.send('/tip ' + '/ ' + req.params.total + ' /' + req.params.tipPercentage);
 });
