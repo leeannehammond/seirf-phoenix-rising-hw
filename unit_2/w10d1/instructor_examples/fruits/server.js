a167eced47e22db431307eabb9eb47e158740c34
Joe Keohan <jkeohan@gmail.com>
2020-02-15 16:34:51 -0500

    added w10d3 homework

--- unit_2/w10d1/instructor_examples/fruits/server.js
+++ unit_2/w10d1/instructor_examples/fruits/server.js
@@ -15,16 +15,16 @@ response
 */
 
 app.use((request, response, next) => {
-    console.log('I run for all routes')
-    // continue with your routing
-    next()
-})
+  console.log("I run for all routes");
+  // continue with your routing
+  next();
+});
 
 // without this middle request.body is undefined
-// with it...it add a body key and sets it to 
+// with it...it add a body key and sets it to
 // whatever was sent in the body
-app.use(express.urlencoded({extended:false}))
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'))
 
 app.get("/fruits", (request, response) => {
   response.render("index.ejs", {
@@ -32,27 +32,44 @@ app.get("/fruits", (request, response) => {
   });
 });
 
-// without this route we would get
-// Cannot POST /fruits
-app.post('/fruits', (request,response) => {
-    console.log('body', request.body)
-    if(request.body.readyToEat === 'on') {
-        request.body.readyToEat = true
-    } else {
-        request.body.readyToEat = false
-    }
+app.post("/fruits", (request, response) => {
+  console.log("body", request.body);
+  if (request.body.readyToEat === "on") {
+    request.body.readyToEat = true;
+  } else {
+    request.body.readyToEat = false;
+  }
 
-    let fruit = {}
-    fruit.name = request.body.name
-    fruit.color = request.body.color
-    fruit.readyToEat = request.body.readyToEat
-    fruits.push(fruit)
+  let fruit = {};
+  fruit.name = request.body.name;
+  fruit.color = request.body.color;
+  fruit.readyToEat = request.body.readyToEat;
+  fruits.push(fruit);
 
-    console.log('fruits', fruits)
-    // redirect here is used to send the user to the
-    // app.get('/fruits') route
-    response.redirect('/fruits')
-})
+  console.log("fruits", fruits);
+  // redirect here is used to send the user to the
+  // app.get('/fruits') route
+  response.redirect("/fruits");
+});
+
+app.post("/products", (request, response) => {
+  console.log("Hit create route");
+  console.log('request.body is: ', request.body)
+//   response.send(request.body);
+  response.json(request.body)
+});
+
+// app.delete("/fruits/:indexOfFruitsArray", (request, response) => {
+//     response.render("show.ejs", {
+//       fruit: fruits[request.params.indexOfFruitsArray]
+//     });
+//   });
+
+// app.put("/fruits/:indexOfFruitsArray", (request, response) => {
+// response.render("show.ejs", {
+//     fruit: fruits[request.params.indexOfFruitsArray]
+// });
+// });
 
 // add a new get route for /fruits/new
 app.get("/fruits/new", (request, response) => {
@@ -65,6 +82,6 @@ app.get("/fruits/:indexOfFruitsArray", (request, response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("I am listening on port 3000");
+app.listen(3001, () => {
+  console.log("I am listening on port 3001");
 });
