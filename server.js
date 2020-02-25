bc0f785924809820d9d1e23d2080bb8afd273f4a
leeannehammond <56901618+leeannehammond@users.noreply.github.com>
2020-02-25 18:55:39 -0500

    w10d3_homework done!
    pokemon.js--- couldn't get all of the info to go to my file. No idea why.

--- server.js
+++ server.js
@@ -1,40 +1,117 @@
-//set up server
-// dependecies
-const express = require('express')
-const app = express()
-const port = 3000
+// packages
+const express = require('express'),
+      methodOverride = require('method-override'),
+      app = express();
 
-const pokemon = require('./models/pokemon.js')
+// database
+const pokemon = require('../pokedex/models/pokemon.js');
 
-//middleware
-app.use(express.static('public'))
+// middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
 
-//route
+
+// data route
+app.get('/pokedata', (req, res) => {
+  res.send(pokemon);
+})
+
+// index route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Pokemon App!')
+  res.render('index.ejs');
 })
 
-// get index
+// pokedex route
 app.get('/pokemon', (req, res) => {
-    for(let poke of pokemon) {
-        let splitName = poke.name.split('')
-        splitName[0] = splitName[0].toUpperCase()
-        poke.name = splitName.join('')
+  res.render('pokedex.ejs', {
+    // sends pokemon data into the pokedex.ejs page under the variable 'data'
+    data: pokemon
+  });
+})
+
+// new route
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+})
+
+// show route
+app.get('/pokemon/:index', (req, res) => {
+  res.render('show.ejs', {
+    // sends the specific pokemon data into the show.ejs page under the variable 'data '
+    data: pokemon[req.params.index],
+    // sends the index of the pokemon into the show.ejs page under the variable 'index'
+    index: req.params.index
+  })
+})
+
+// edit route
+app.get('/pokemon/:index/edit', (req, res) => {
+  res.render('edit.ejs', {
+    // sends the specific pokemon data into the show.ejs page under the variable 'data '
+    data: pokemon[req.params.index],
+    // sends the index of the pokemon into the show.ejs page under the variable 'index'
+    index: req.params.index
+  })
+})
+
+
+// post route
+app.post('/pokemon', (req, res) => {
+  console.log(req.body);
+  // creates newPokemon object to match the data structure of the model
+  let newPokemon = {
+    name: req.body.name,
+    img: req.body.img,
+    type: req.body.type.split(','),
+    stats: {
+      hp: req.body.hp,
+      attack: req.body.attack,
+      defense: req.body.defense,
+      spattack: req.body.spattack,
+      spdefense: req.body.spdefense,
+      speed: req.body.speed
     }
-    //render
-    res.render('index.ejs', {
-        pokemon: pokemon
-    })
+  };
+  console.log(newPokemon);
+  // pushes the newPokemon object into the databse
+  pokemon.push(newPokemon);
+  // redirects to the pokedex
+  res.redirect('/pokemon' );
 })
 
-// show certain pokemon
-app.get('/pokemon/:indexOfPokemon', (req, res) => {
-    res.render('show.ejs', {
-        pokemon: pokemon[req.params.indexOfPokemon]
-    })
+// put route
+app.put('/pokemon/:index', (req, res) => {
+  console.log(req.body);
+  // creates editPokemon object to match the data structure of the model
+  let editPokemon = {
+    name: req.body.name,
+    img: req.body.img,
+    type: req.body.type.split(','),
+    stats: {
+      hp: req.body.hp,
+      attack: req.body.attack,
+      defense: req.body.defense,
+      spattack: req.body.spattack,
+      spdefense: req.body.spdefense,
+      speed: req.body.speed
+    }
+  }
+  console.log(editPokemon);
+  // finds the pokemon we're editing by the index number, then sets it equal to the editPokemon object
+  pokemon[req.params.index] = editPokemon;
+  res.redirect('/pokemon/' + req.params.index);
 })
 
-//listeners
-app.listen(port, () => {
-    console.log(`connected to poke express! on port:${port}`)
+// delete route
+app.delete('/pokemon/:index', (req, res) => {
+  // splices the pokemon from the 'database' based on its array index
+  pokemon.splice(req.params.index, 1);
+  // redirects to pokedex
+  res.redirect('/pokemon');
+})
+
+// listener
+app.listen(3000, () => {
+  console.log('Gotta catch em all on port 3000');
 })
