dd988136e39e46c0a55a767016d6a7406c1c4db0
leeannehammond <56901618+leeannehammond@users.noreply.github.com>
2020-02-26 21:20:22 -0500

    log.js created

---
+++ seir-flex/student_labs_folder/captains_log/models/logs.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const logsSchema = new mongoose.Schema({
+    name:  { type: String, required: true },
+    entry:  { type: String, required: true },
+    shipIsBroken: Boolean
+    timestamps: {type: Boolean, required: true}
+});
+
+const Logs = mongoose.model('Logs', fruitSchema);
+
+module.exports = Logs;
+
--- seir-flex/student_labs_folder/captains_log/server.js
+++ seir-flex/student_labs_folder/captains_log/server.js
@@ -1,13 +1,45 @@
 // start express
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-app.listen(3000, ()=>{
-    console.log('listening');
+//... and then farther down the file
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
 });
 
 // new route
 app.get('/logs/new', (req, res)=>{
-    res.render('new');
+    res.send('new');
 });
 
+//render
+app.get('/fruits/new', (req, res)=>{
+    res.render('new.ejs');
+});
+
+// create
+app.post('/logs/', (req, res)=>{
+    res.send('received');
+});
+
+// parse
+app.use(express.urlencoded({extended:true}));
+app.post('/logs/', (req, res)=>{
+    res.send(req.body);
+});
+
+// upgrade data
+app.post('/logs/', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    res.send(req.body);
+});
+
+app.listen(3000, ()=>{
+    console.log('listening');
+});
