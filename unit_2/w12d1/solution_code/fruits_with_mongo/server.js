2d052ce3b5b6ee5bba0a1c0ac5a777528539a425
Joe Keohan <jkeohan@gmail.com>
2020-02-29 16:20:03 -0500

    migrated fruits routes to supporting controller

--- unit_2/w12d1/solution_code/fruits_with_mongo/server.js
+++ unit_2/w12d1/solution_code/fruits_with_mongo/server.js
@@ -17,111 +17,19 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongo");
 });
 
-const Fruit = require("./models/fruits.js");
+const fruitsController = require('./controllers/fruits.js')
+// any routes that come in for fruits should be sent
+// to the fruitsContoller
+app.use('/fruits', fruitsController)
 
-// Routes:
+app.get('/', (req, res) => {
+  res.redirect('/fruits')
+})
 
-// NEW
-app.get("/fruits/new", (req, res) => {
-  res.render("new.ejs");
-});
-
-// CREATE
-app.post("/fruits", (req, res) => {
-  if (req.body.readyToEat === "on") {
-    //if checked, req.body.readyToEat is set to 'on'
-    req.body.readyToEat = true;
-  } else {
-    //if not checked, req.body.readyToEat is undefined
-    req.body.readyToEat = false;
-  }
-  Fruit.create(req.body, (error, result) => {
-    // res.send(result);
-    res.redirect("/fruits");
-  });
-});
-
-// INDEX..aka SHOW ALL
-app.get("/fruits", (req, res) => {
-  Fruit.find({}, (error, fruits) => {
-    // res.send(fruits);
-    res.render("index.ejs", { fruits });
-  });
-});
-
-// SECRET SEED ROUTE
-app.get("/fruits/seed", (req, res) => {
-  Fruit.create(
-    [
-      {
-        name: "grapefruit",
-        color: "pink",
-        readyToEat: true
-      },
-      {
-        name: "grape",
-        color: "purple",
-        readyToEat: false
-      },
-      {
-        name: "avocado",
-        color: "green",
-        readyToEat: true
-      }
-    ],
-    (error, data) => {
-      console.log(data);
-      res.redirect("/fruits");
-    }
-  );
-});
-
-// SHOW ONE
-app.get("/fruits/:id", (req, res) => {
-  Fruit.findById(req.params.id, (err, foundFruit) => {
-    res.render("show.ejs", {
-      fruit: foundFruit
-    });
-  });
-});
-
-// DELETE
-app.delete("/fruits/:id", (req, res) => {
-  // res.send('deleting...')
-  Fruit.findByIdAndRemove(req.params.id, (err, data) => {
-    res.redirect("/fruits");
-  });
-});
-
-// EDIT
-// /fruits/5e5a93cd12675b4c0efcb17e/edit
-app.get("/fruits/:id/edit", (req, res) => {
-  Fruit.findById(req.params.id, (err, foundFruit) => {
-    console.log("foundFruit", foundFruit);
-    res.render("edit.ejs", {
-      fruit: foundFruit
-    });
-  });
-});
-
-// PUT/UPDATE
-app.put("/fruits/:id", (req, res) => {
-  if (req.body.readyToEat === "on") {
-    req.body.readyToEat = true;
-  } else {
-    req.body.readyToEat = false;
-  }
-  // res.send(req.body)
-  Fruit.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, updateModel) => {
-      // res.send(updateModel);
-      res.redirect('/fruits')
-    }
-  );
-});
+// wildcard route
+app.get('*', (req, res) => {
+  res.redirect('/fruits')
+})
 
 // Web server:
 app.listen(3000, () => {
