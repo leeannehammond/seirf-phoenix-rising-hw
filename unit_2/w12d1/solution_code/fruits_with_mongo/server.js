418c3ecd697b305853b98d6a19d7323841c0e55d
Joe Keohan <jkeohan@gmail.com>
2020-02-29 17:24:50 -0500

    added express router

--- unit_2/w12d1/solution_code/fruits_with_mongo/server.js
+++ unit_2/w12d1/solution_code/fruits_with_mongo/server.js
@@ -7,6 +7,11 @@ const mongoose = require("mongoose");
 // allows server to review json data
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+// your own custom middleware
+app.use((req, res, next) => {
+  console.log("my own middleware");
+  next();
+});
 
 // Connect mongoose to mongo db:
 mongoose.connect("mongodb://localhost:27017/fruitsdb", {
@@ -17,19 +22,19 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongo");
 });
 
-const fruitsController = require('./controllers/fruits.js')
+const fruitsController = require("./controllers/fruits.js");
 // any routes that come in for fruits should be sent
 // to the fruitsContoller
-app.use('/fruits', fruitsController)
+app.use("/fruits", fruitsController);
 
-app.get('/', (req, res) => {
-  res.redirect('/fruits')
-})
+app.get("/", (req, res) => {
+  res.redirect("/fruits");
+});
 
 // wildcard route
-app.get('*', (req, res) => {
-  res.redirect('/fruits')
-})
+app.get("*", (req, res) => {
+  res.redirect("/fruits");
+});
 
 // Web server:
 app.listen(3000, () => {
