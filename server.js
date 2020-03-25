e40169ef5fe3555ced7e264b952486236a6a7557
leeannehammond <56901618+leeannehammond@users.noreply.github.com>
2020-03-25 16:24:17 -0400

    BUDGETR

--- server.js
+++ server.js
@@ -1,113 +1,46 @@
-////////////// Greetings //////////////////////
-
-// make a route '/greeting' sending greeting
-
-// Dependencies
-const express = require('express');
-const app = express();
-
-// listen for when someone goes to
-// localhost:2000/
-
-
-const express = require('express'); //from documentation: express is function
-const app = express();//app is an object
-app.get('/greeting', (req, res)=>{
-  res.send('Hello Stranger!');
-});
-
-app.get('/greeting/'), (req,res) => {
-  res.send('What is up, <name>')
-};
-});
-
-app.listen(2000, () => {
-    console.log("Wow! Hello there");
-});
-
-//////////////// Tip Calculator ////////////////
-
-// app route of '/tip' w/2 params of total and tipPercentage
-// hit route to display how much tip is w/total amt and tip amt
-app.get('/ti[\p/:total/:tipPercentage', (req, res) => {
-	console.log(req.params);
-	res.send('/tip ' + '/ ' + req.params.total + ' /' + req.params.tipPercentage);
-});
- 
-////////////// Magic 8 Ball ////////////////////
-// route magic to expect phrase of url for 8ball
-// return responses from array when asked question
-// h1 of 8 ball response
-
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-const responseBack = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
-"You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", 
-"Signs point to yes", "Reply hazy try again", "Ask again later",
-"Better not tell you now", "Cannot predict now", "Concentrate and ask again",
-"Don't count on it", "My reply is no", "My sources say no","Outlook not so good", 
-"Very doubtful"]
+// requires
+const items = require("./models/budget.js");
 
-app.get('/magic', (req, res) => {
-  res.send(`
-    <h1>Magic 8 ball response</h1>
-  `);
+// body parser
+app.use(express.urlencoded({extended:false})); 
+
+// static
+app.use(express.static('public'));
+
+// routes
+app.get('/budget',(req,res) => {                            // index
+    res.render('index.ejs',{items});
 });
 
-app.get('/:'/magic/Will%20I%20Be%20A%20Millionaire'', (req, res) => {
-    res.send(magic[req.params.responseBack]);
+app.get('/budget/new',(req,res) => {                        // new route
+    res.render('new.ejs');
 });
 
-app.listen(port,() => {
-    console.log('listening on port' , port);
+app.get('/budget/:id',(req,res) => {                        // show route
+    res.render('show.ejs',{item: items[req.params.id]});
 });
 
-///////////// Take one Down and Pass it Around ////////////////////////
-// users should see 99 bottles of beer on the wall
-// link to take one down, pass it around
-// when number is in url, use should see number of bottles on wall and
-// link to take one down
-// if 0 bottles left, do not show a link-add link to start over. Direct user
-// back to homepage. 
+app.post("/budget", (req,res) => {
+    console.log(req.body);
+    let item = {}
+    item.name = req.body.name;
+    item.date = req.body.date;
+    item.amount = Number(req.body.amount);
+    item.tags = req.body.tags;
+    items.push(item);
+    res.redirect("/budget");
+});
 
-var express = require('express')
-var app = express()
-var port = 3001
 
-app.get('/', (req, res) => {
-  res.send(`
-    <html>
-    <body>
-      <h1>99 bottles of beer on the wall</h1>
-      <a href ="/98">take one down and pass it around</a>
-    </body>
-    </html>`
-  )
-})
 
-app.get('/:number_of_bottles', (req, res) => {
-  let bottleNum = parseInt(req.params.number_of_bottles)
-  let takeOneDown = parseInt(bottleNum -1)
-  if (bottleNum > 0) {
-    res.send(`
-    <html>
-    <body>
-    <h1>${bottleNum} bottles of beer on the wall</h1>
-    <a href="http://localhost:3001/${takeOneDown}">take one down and pass it around</a>
-    </body>
-    </html>`)
-  } else if (bottleNum === 0) {
-    res.send(`<html>
-    <body>
-    <h1>${bottleNum} bottles of beer on the wall</h1>
-    <a href="/">Start over</a>
-    </body>
-    </hmtl>`)
-  }
-})
 
+
+
+// server listen
 app.listen(port, () => {
-  console.log('app is running on port: ', port)
-})
+    console.log('budget server is running');
+});
