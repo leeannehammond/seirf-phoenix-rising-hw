470de2fd9ec4ec88a4fe3aa57e8585902c60689c
leeannehammond <56901618+leeannehammond@users.noreply.github.com>
2020-04-20 17:28:17 -0400

    REACT_TACO

--- app.js
+++ app.js
@@ -1,113 +1,52 @@
-class DivThree extends React.Component {
-    render() {
-        return(
-            <div onClick={() => this.props.func(this.props.state.name)}>
-                <h3>{this.props.state.name}</h3>
-            </div>
-        )
+class RandomTaco extends React.Component {
+    render () {
+      return  (
+        <div>
+          <h1>Shell: {this.props.taco.shell.slug}</h1>
+          <p>{this.props.taco.shell.recipe}</p>
+  
+          <h1>Mixin: {this.props.taco.mixin.slug}</h1>
+          <p>{this.props.taco.mixin.recipe}</p>
+  
+          <h1>Condiment: {this.props.taco.condiment.slug}</h1>
+          <p>{this.props.taco.condiment.recipe}</p>
+          
+          <h1>Seasoning: {this.props.taco.seasoning.slug}</h1>
+          <p>{this.props.taco.seasoning.recipe}</p>
+  
+          <h1>Base layer: {this.props.taco.base_layer.slug}</h1>
+          <p>{this.props.taco.base_layer.recipe}</p>
+    
+        </div>
+      )
     }
-}
-
-// DIVTWO CLASS
-class DivTwo extends React.Component {
-    render() {
-        return (
-            <div >
-                <DivThree 
-                    state={this.props.state1}
-                    func={this.props.func1}
-                />
-                <DivThree 
-                    state={this.props.state2}
-                    func={this.props.func2}
-                />
-            </div>
-        )
-    }
-}
-
-// DIVONE CLASS
-class DivOne extends React.Component {
-    render() {
-        return (
-            <div>
-                <DivTwo 
-                    state1={this.props.state1}
-                    state2={this.props.state2}
-                    func1={this.props.func1}
-                    func2={this.props.func2}
-                />
-            </div>
-        )
-    } 
-}
-
-// APP CLASS
-class App extends React.Component {
+  }
+  
+  class App extends React.Component {
+      
     state = {
-        tardis1: {
-            name: 'Time and Relative Dimension in Space, State 1',
-            caps: false,
-        },
-        tardis2: {
-            name: 'Time and Relative Dimension in Space, State 2',
-            caps: false,
-        }
+        URL: "https://taco-randomizer.herokuapp.com/random/?full-tack=true",
     }
-    // function to change tardis1
-    changeTardis1 = (text) => {
-        console.log("function 1 called");
-        if(this.state.tardis1.caps){
-            this.setState({
-                tardis1: {
-                    name: text.toLowerCase(),
-                    caps: false
-                }
-            })
-        } else {
-            this.setState({
-                tardis1: {
-                    name: text.toUpperCase(),
-                    caps: true
-                }
-            })
-        }
-    }   
-    // function to change tardis2
-    changeTardis2 = (text) => {
-        console.log("function 2 called");
-        if(this.state.tardis2.caps){
-            this.setState({
-                tardis2: {
-                    name: text.toLowerCase(),
-                    caps: false
-                }
-            })
-        } else {
-            this.setState({
-                tardis2: {
-                    name: text.toUpperCase(),
-                    caps: true
-                }
-            })
-        }
+  
+    componentDidMount = () => {
+      fetch("https://taco-randomizer.herokuapp.com/random/?full-tack=true")
+        .then(response => response.json())
+        .then(data => this.setState({ taco: data }));
     }
-    render() { 
-        return (
-            <div>
-                <DivOne 
-                    func1={this.changeTardis1}
-                    func2={this.changeTardis2}
-                    state1={this.state.tardis1}
-                    state2={this.state.tardis2}
-                />
-            </div>
-        )
-    }
-}
-
-// REACT RENDER
-ReactDOM.render(
-    <App />,
-    document.querySelector(".container")
-)
+  
+    render() {
+      console.log(this.state)
+      return (
+        <React.Fragment>
+          <h1>Random Taco Tuesday</h1>
+          {this.state.taco ? <RandomTaco taco={this.state.taco}/> : ""}
+        </React.Fragment>
+      )
+      }
+  }
+  
+  
+  ReactDOM.render(
+      <App/>, 
+      document.querySelector(".container")
+  )
